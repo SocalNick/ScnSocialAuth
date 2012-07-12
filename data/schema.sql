0bca8f31eb061509a2424a30ef43627d3ffae02e
Nicholas Calugar <njcalugar@gmail.com>
2012-07-12 10:26:12 -0700

    Yahoo is 26 alphanumeric chars so bumping to varchar(50) to be safe

--- data/schema.sql
+++ data/schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS `user_provider` (
   `user_id` int(11) NOT NULL,
-  `provider_id` varchar(25) NOT NULL,
+  `provider_id` varchar(50) NOT NULL,
   `provider` varchar(255) NOT NULL,
   PRIMARY KEY (`user_id`),
   UNIQUE KEY (`provider_id`,`provider`),
