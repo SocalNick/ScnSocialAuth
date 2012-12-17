7414fb71ef0b1976c814f8a93040cf15f9f03d3a
Nicholas Calugar <njcalugar@gmail.com>
2012-12-17 09:53:42 -0800

    Getting some basic tests working

---
+++ test/Bootstrap.php
@@ -0,0 +1,72 @@
+<?php
+namespace ScnSocialAuthTest;
+
+use Zend\Loader\AutoloaderFactory;
+use RuntimeException;
+
+error_reporting(E_ALL | E_STRICT);
+chdir(__DIR__);
+
+class Bootstrap
+{
+    protected static $serviceManager;
+
+    public static function init()
+    {
+        static::initAutoloader();
+    }
+
+    protected static function initAutoloader()
+    {
+        $vendorPath = static::findParentPath('vendor');
+        echo $vendorPath . "\n";
+
+        if (is_readable($vendorPath . '/autoload.php')) {
+            $loader = include $vendorPath . '/autoload.php';
+        }
+
+        $zf2Path = getenv('ZF2_PATH') ?: (defined('ZF2_PATH') ? ZF2_PATH : (is_dir($vendorPath . '/zendframework/zendframework/library') ? $vendorPath . '/zendframework/zendframework/library' : false));
+
+        if (!$zf2Path) {
+            throw new RuntimeException('Unable to load ZF2. Run `php composer.phar install` or define a ZF2_PATH environment variable.');
+        }
+
+        if (isset($loader)) {
+            $loader->add('Zend', $zf2Path . '/Zend');
+        } else {
+            include $zf2Path . '/Zend/Loader/AutoloaderFactory.php';
+            include $zf2Path . '/Zend/Loader/ClassMapAutoloader.php';
+            AutoloaderFactory::factory(array(
+                'Zend\Loader\ClassMapAutoloader' => array(array(
+                    'ScnSocialAuth\Module' => __DIR__ . '/../Module.php',
+                )),
+                'Zend\Loader\StandardAutoloader' => array(
+                    'autoregister_zf' => true,
+                    'namespaces' => array(
+                        'ScnSocialAuth' => __DIR__ . '/../src/ScnSocialAuth',
+                        __NAMESPACE__ => __DIR__ . '/' . __NAMESPACE__,
+                    ),
+                ),
+            ));
+        }
+    }
+
+    protected static function findParentPath($path)
+    {
+        $dir = __DIR__;
+        $previousDir = '.';
+        while (!is_dir($dir . '/' . $path)) {
+            echo $dir . '/' . $path . "\n";
+            $dir = dirname($dir);
+            echo $dir . '/' . $path . "\n";
+            if ($previousDir === $dir) {
+                return false;
+            }
+            $previousDir = $dir;
+        }
+
+        return $dir . '/' . $path;
+    }
+}
+
+Bootstrap::init();
---
+++ test/ScnSocialAuthTest/UserControllerTest.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * @link      https://github.com/SocalNick/ScnSocialAuth for the canonical source repository
+ * @copyright Copyright (c) 2012 Nicholas Calugar (http://socalnick.github.com)
+ */
+
+namespace ScnSocialAuthTest;
+
+use ScnSocialAuth\Controller\UserController;
+use PHPUnit_Framework_TestCase as TestCase;
+use Zend\Mvc\MvcEvent;
+use Zend\Stdlib\Request;
+
+/**
+ * Unit tests for PhlySimplePage\PageController
+ */
+class PageControllerTest extends TestCase
+{
+    public function setUp()
+    {
+        $this->event      = new MvcEvent();
+        $this->controller = new UserController();
+        $this->controller->setEvent($this->event);
+    }
+
+    public function testIsEventManagerAware()
+    {
+        $this->assertInstanceOf('Zend\EventManager\EventManagerAwareInterface', $this->controller);
+    }
+
+    public function testIsDispatchable()
+    {
+        $this->assertInstanceOf('Zend\Stdlib\DispatchableInterface', $this->controller);
+    }
+
+    public function testIsEventInjectable()
+    {
+        $this->assertInstanceOf('Zend\Mvc\InjectApplicationEventInterface', $this->controller);
+    }
+
+    public function testRaisesExceptionOnDispatchIfEventDoesNotContainRouteMatch()
+    {
+        $request = new Request();
+        $this->setExpectedException('Zend\Mvc\Exception\DomainException', 'Missing route matches');
+        $this->controller->dispatch($request);
+    }
+}
