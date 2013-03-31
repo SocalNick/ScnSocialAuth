56e76bdd1d241f0d63cdb07a1b79fe6b67512176
Nicholas Calugar <njcalugar@gmail.com>
2013-03-31 16:25:40 -0700

    Removing query route that is no longer available since ZF2 2.1.4

--- test/ScnSocialAuthTest/Controller/UserControllerTest.php
+++ test/ScnSocialAuthTest/Controller/UserControllerTest.php
@@ -8,19 +8,67 @@ namespace ScnSocialAuthTest\Controller;
 
 use ScnSocialAuth\Controller\UserController;
 use PHPUnit_Framework_TestCase as TestCase;
+use ScnSocialAuth\Options\ModuleOptions;
+use Zend\Mvc\Controller\PluginManager;
 use Zend\Mvc\MvcEvent;
-use Zend\Stdlib\Request;
+use Zend\Http\PhpEnvironment\Request;
+use Zend\ServiceManager\ServiceManager;
 
-/**
- * Unit tests for PhlySimplePage\PageController
- */
-class PageControllerTest extends TestCase
+class UserControllerTest extends TestCase
 {
+    /**
+     * @var \Zend\ServiceManager\ServiceManager
+     */
+    protected $sm;
+
+    /**
+     * @var \Zend\Mvc\Controller\PluginManager
+     */
+    protected $pm;
+
+    /**
+     * @var \ScnSocialAuth\Controller\UserController
+     */
+    protected $controller;
+
+    /**
+     * @var \Zend\Mvc\MvcEvent;
+     */
+    protected $event;
+
+    /**
+     * @var \Zend\Http\PhpEnvironment\Request
+     */
+    protected $request;
+
     public function setUp()
     {
-        $this->event      = new MvcEvent();
+        $this->sm = new ServiceManager();
+        $this->pm = new PluginManager();
+        $this->event = new MvcEvent();
+        $this->request = new Request();
         $this->controller = new UserController();
         $this->controller->setEvent($this->event);
+        $this->controller->setServiceLocator($this->sm);
+        $this->controller->setPluginManager($this->pm);
+
+        $forwardPlugin = \Mockery::mock('Zend\Mvc\Controller\Plugin\Forward[dispatch]');
+        $this->pm->setService('forward', $forwardPlugin);
+    }
+
+    public function tearDown()
+    {
+        \Mockery::close();
+    }
+
+    protected function dispatch($action, $params = array())
+    {
+        $routeMatch = new \Zend\Mvc\Router\RouteMatch(array_merge(array('action' => $action), $params));
+        $this->event->setRouteMatch($routeMatch);
+        $this->controller->setEvent($this->event);
+        $this->controller->dispatch($this->request);
+
+        return $this->event->getResult();
     }
 
     public function testIsEventManagerAware()
@@ -44,4 +92,24 @@ class PageControllerTest extends TestCase
         $this->setExpectedException('Zend\Mvc\Exception\DomainException', 'Missing route matches');
         $this->controller->dispatch($request);
     }
+
+    public function testLoginProxiesToZfcUserAndReturnsNonModelInterface()
+    {
+        /** @var $forwardPlugin \Mockery\MockInterface */
+        $forwardPlugin = $this->pm->get('forward');
+        $forwardPlugin->shouldReceive('dispatch')
+            ->with('zfcuser', array('action' => 'login'))
+            ->andReturn('zfc-user-login');
+
+        $result = $this->dispatch('login');
+        $this->assertEquals('zfc-user-login', $result);
+    }
+
+    public function testProviderLoginInvalidProvider()
+    {
+        $this->controller->setOptions(new ModuleOptions());
+        $result = $this->dispatch('provider-login', array('provider' => 'facebook'));
+        $this->assertEquals('Page not found', $result->content);
+    }
 }
+
