d3afadec2e19fc46a7cd60b8edac340a9310a225
Nicholas Calugar <njcalugar@gmail.com>
2013-04-12 09:04:03 -0700

    Update schema.sql
    user_id column should match the definition from ZfcUser

--- data/schema.sql
+++ data/schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS `user_provider` (
-  `user_id` int(11) NOT NULL,
+  `user_id` INT UNSIGNED NOT NULL,
   `provider_id` varchar(50) NOT NULL,
   `provider` varchar(255) NOT NULL,
   PRIMARY KEY (`user_id`,`provider_id`),
