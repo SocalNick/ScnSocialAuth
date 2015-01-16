4a00966c41bc37251586d007564c5c891eba3700
Nicholas Calugar <njcalugar@gmail.com>
2015-01-15 22:44:44 -0800

    Fix XSS vulnerability
    - This was originally demonstrated in ZfcUser but ScnSocialAuth
    was also not properly escaping the redirect query param for social
    sign-in buttons.
    - Fixed by passing a $options array to the ZF2 Url View Helper.

--- src/ScnSocialAuth/View/Helper/SocialSignInButton.php
+++ src/ScnSocialAuth/View/Helper/SocialSignInButton.php
@@ -7,10 +7,20 @@ class SocialSignInButton extends AbstractHelper
 {
     public function __invoke($provider, $redirect = false)
     {
-        $redirectArg = $redirect ? '?redirect=' . $redirect : '';
-        echo
-            '<a class="btn" href="'
-            . $this->view->url('scn-social-auth-user/login/provider', array('provider' => $provider))
-            . $redirectArg . '">' . ucfirst($provider) . '</a>';
+        $name = 'scn-social-auth-user/login/provider';
+        $params = array('provider' => $provider);
+        $options = array();
+
+        if ($redirect) {
+            $options = array(
+                'query' => array(
+                    'redirect' => $redirect,
+                ),
+            );
+        }
+
+        $url = $this->view->url($name, $params, $options);
+
+        echo '<a class="btn" href="' . $url . '">' . ucfirst($provider) . '</a>';
     }
 }
